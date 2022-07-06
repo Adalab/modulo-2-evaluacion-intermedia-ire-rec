2c4ab0d5147025790ca7b5c558b695f3566bcb4c
ire-rec <i.recavarren@gmail.com>
2022-07-06 10:04:16 +0200

    add HTML estructure, dcq and prevent default

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const btn = document.querySelector('.js-btn');
+const choose = document.querySelector('.js-choose');
+const writeNumberP1 = document.querySelector('.js-writeNumberP1');
+const numberAttemptP2 = document.querySelector('.js-numberAttemptP2');
+
+
+function handleClick(ev){
+    ev.preventDefault();
+    console.log("Ye! Funciona")
+}
+
+
+btn.addEventListener("click", handleClick);
