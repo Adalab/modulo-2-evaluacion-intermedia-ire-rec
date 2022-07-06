88d4c4c7007932ad133931cc69d303977cc6c92f
ire-rec <i.recavarren@gmail.com>
2022-07-06 11:05:20 +0200

    add final

--- JS/main.js
+++ JS/main.js
@@ -6,9 +6,26 @@ const writeNumberP1 = document.querySelector('.js-writeNumberP1');
 const numberAttemptP2 = document.querySelector('.js-numberAttemptP2');
 
 
+const chooseNumber=()=>{
+if(chooseValue===getRandomNumber);
+else('Has ganado campeona!!!');
+}
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);       
+   }
+
+function pickUpValue(){
+    const chooseValue = choose.value;
+    console.log(chooseValue);    
+}
+
 function handleClick(ev){
     ev.preventDefault();
-    console.log("Ye! Funciona")
+    pickUpValue();
+    getRandomNumber(100);
+
 }
 
 
